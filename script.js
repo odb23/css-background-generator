88fc7158bd94eeb8c0c72ba46d7adad978c5629c
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2021-11-23 18:43:33 +0100

    adding color randomization

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+var getRandomColor = ()=> "#" + Math.floor(Math.random() * 16777215).toString();
+
+function generateColors () {
+    color1.value = getRandomColor();
+    color2.value = getRandomColor();
+}
+
+
 function updateGradient () {
     body.style.background = "linear-gradient(to " 
                         + "right" + ", " 
@@ -12,6 +20,7 @@ function updateGradient () {
     h3.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", updateGradient);
 
+generateColors();
+color1.addEventListener("input", updateGradient);
 color2.addEventListener("input", updateGradient);
